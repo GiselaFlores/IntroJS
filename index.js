d1921f585e7c99e747db793ff0f5a82cee9f43bf
GiselaFlores <gi.flores@bue.edu.ar>
2023-09-29 19:46:25 -0300

    variables

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let nombre = prompt("Ingresa tu nombre:");
+console.log(nombre);
+
+let edad = parseInt(prompt("Ingresa tu edad:"));
+console.log(edad);
+
+alert("Hola bienvenida");
+
+let _variableNumero1 = "Hola";
+const PI = 3.14;
+
+let nombre1 = 23, nombre2, nombre3 = "hola";
+
+let nombre4 = 23;
+let nombre5;
+let nombre6 = "hola";
+
+let objeto = {
+    propiedad: "valor",
+    propiedad2: 23,
+    propiedad3: "chau"
+};
+
+nombre2 = nombre6 +" "+ objeto.propiedad + nombre4;
+
+console.log(nombre2.length);
+
+console.log(nombre2.toUpperCase());
+
+console.log(nombre2.toLowerCase());
+
+console.log(nombre2.repeat(5));
+
+console.log(nombre2.replace(nombre6, objeto.propiedad3));
